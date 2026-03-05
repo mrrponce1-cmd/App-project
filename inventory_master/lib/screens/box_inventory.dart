e34af5a01d4ab95c1b93cfca751b1661e22c5f4d
unknown <mrrponce1@tip.edu.ph>
2026-03-05 17:28:38 +0800

    Fix log-in page

--- inventory_master/lib/screens/box_inventory.dart
+++ inventory_master/lib/screens/box_inventory.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class BoxInventoryPage extends StatelessWidget {
+  const BoxInventoryPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- inventory_master/lib/screens/retail_inventory.dart
+++ inventory_master/lib/screens/retail_inventory.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RetailInventoryPage extends StatelessWidget {
+  const RetailInventoryPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
